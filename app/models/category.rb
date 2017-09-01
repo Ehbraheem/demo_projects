dcdee0f50a5ec2cfc38c9b8a3879d7f09fbee043
Ehbraheem <ehbraheem@gmail.com>
2017-09-01 01:30:42 -0700

    Callbacks

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,20 @@
 class Category < ApplicationRecord
 
 	has_many :products
-	
+
+	validates :name, presence: true
+	validates :prefix, presence: true
+
+	before_validation :add_default_prefix, if: Proc.new { |cat| cat.prefix.blank? }
+
+	# around_save :benchmark
+
+	private
+
+		def add_default_prefix
+			puts "*** Adding default prefix! ***"
+			self.prefix = 'GEN'
+		end
+
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,10 +6,18 @@ class Product < ApplicationRecord
 
 	scope :recent, -> { where(["created_at >= ?", 1.year.ago]) }
 
+	after_save :announce_save
+
 	delegate :prefix, to: :category, allow_nil: true
 
 	def full_ref_num
 		"#{prefix}-#{ref_num}"
 	end
+
+	private
+
+		def announce_save
+			puts "*** Saved! ***"
+		end
 
 end
